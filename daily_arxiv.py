f00e035c96c6ab3ab00a7996e941cc9e5d94e675
Elvis Zhang <17877010+zjs1224522500@users.noreply.github.com>
2021-10-26 22:08:57 +0800

    Update daily_arxiv.py

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -180,7 +180,7 @@ if __name__ == "__main__":
     
     keywords = dict()
     keywords["LSM"]                = "LSM-Tree" + "OR" + "\"LSM Tree\""
-    keywords["KVS"]                = "KVS" + "AND" + "\"Key-Value Store\"" 
+    keywords["KVS"]                = "\"Key-Value Store\"" 
     keywords["PM-FS"]              = "\"Persistent Memory File System\""
 #    keywords["SFM"]                 = "SFM"+"OR"+"\"Structure from Motion\""
 #    keywords["Visual Localization"] = "\"Camera Localization\"OR\"Visual Localization\"OR\"Camera Re-localisation\""
